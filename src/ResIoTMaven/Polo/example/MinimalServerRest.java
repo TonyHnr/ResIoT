41ae26d091b3cd0d5aec9c95348fd6d6aa5b45c0
Tony <tonyo35@hotmail.fr>
2020-05-13 09:41:51 +0200

    up serverc src

--- src/ResIoTMaven/Polo/example/MinimalServerRest.java
+++ src/ResIoTMaven/Polo/example/MinimalServerRest.java
@@ -1,4 +1,4 @@
-package ResIotMaven.Polo.example.server;
+package ResIoTMaven.Polo.example;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -7,9 +7,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class MinimalServerRest {
 
